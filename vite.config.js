c80b7ced041668677ec21c23ea1fc204f6c9a343
Keshav K Das <94295810+keshavkdas@users.noreply.github.com>
2025-06-17 19:55:31 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: '/anime-hub/',
+    base: 'https://keshavkdas.github.io/anime-hub/',
     build: {
       outDir: 'dist',
     },
