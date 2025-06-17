f09d97ecdb2e8fe44c90f3f0c952bf0f64db9835
Keshav K Das <94295810+keshavkdas@users.noreply.github.com>
2025-06-17 22:32:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+  const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: 'https://keshavkdas.github.io/anime-hub/',
-    build: {
-      outDir: 'dist',
-    },
+    base: '/anime-hub/',
     define: {
-      __MANGADEX_TOKEN__: JSON.stringify(env.VITE_MANGADEX_SECRET),
+      __MANGADEX_TOKEN__: JSON.stringify(env.VITE_MANGADEX_SECRET)
+    },
+    build: {
+      outDir: 'dist'
     }
   };
 });
