b9be8fc3c8fa361ebfeffd824d84ffa13f3ac0ec
Keshav K Das <94295810+keshavkdas@users.noreply.github.com>
2025-06-17 18:54:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "https://keshavkdas.github.io/anime-hub/"
+  base: '/anime-hub/', // set your repo name here
   build: {
     outDir: 'dist'
   }
